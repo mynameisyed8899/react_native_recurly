e4ec3ae6ab2bb4acb77e4aa96c63e077672d9052
mynameisyed8899 <150600083+mynameisyed8899@users.noreply.github.com>
2026-04-17 13:29:16 +0700

    implement fonts

--- Desktop/React Native Course/recurlly 4/Recurly/app/(tabs)/index.tsx
+++ Desktop/React Native Course/recurlly 4/Recurly/app/(tabs)/index.tsx
@@ -8,29 +8,13 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-       <Link href="/onboarding" className="mt-4 reounded bg-primary text-white p-4" >
+      <Text className="text-7xl font-extrabold">Home</Text>
+       <Link href="/onboarding" className="mt-4 reounded bg-primary font-bold  text-white p-4" >
       Go to Onboarding</Link> 
-      <Link href="/(auth)/sign_in" className="mt-4 reounded bg-primary text-white p-4" >
+      <Link href="/(auth)/sign_in" className="mt-4 reounded bg-primary font-bold text-white p-4" >
       Go to Sign In</Link> 
-
-         <Link href="/(auth)/sign_up" className="mt-4 reounded bg-primary text-white p-4" >
+         <Link href="/(auth)/sign_up" className="mt-4 reounded bg-primary font-bold text-white p-4" >
       Go To Sign Up</Link> 
-
-         <Link href="/subscriptions/spotify" className="mt-4 reounded bg-primary text-white p-4" >
-      Spotify Subscription</Link> 
-
-      <Link href={{
-
-        pathname: "/subscriptions/[id]",
-        params: {id: "cloude"},
-      }}>
-        Claude Max Subscription
-      </Link>
-
-        
     </SafeAreaView>
   );
 }
